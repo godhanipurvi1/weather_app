8e668fd3349387692927051191fac413cba059cb
godhanipurvi1 <purvipatel087@gmail.com>
2024-12-19 16:47:47 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:weather/pages/home_page.dart';
-import 'package:weather/pages/saved_location_page.dart';
 import 'package:weather/pages/splash_page.dart';
-import 'package:weather/pages/weather_page.dart';
 import 'package:weather/provider/weather_provider.dart';
 
 void main() {
